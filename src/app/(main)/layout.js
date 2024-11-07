2075cab876167e308d02e56a37276dd6db54030f
jasoncala <calaj@uwindsor.ca>
2024-11-07 12:29:07 -0500

    Created base layout

---
+++ src/app/(main)/layout.js
@@ -0,0 +1,14 @@
+export default function MainLayout ({ children }) {
+  return (
+    <div className="flex flex-col h-screen">
+      {/* Header here */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* Sidebar here */}
+        <div className="flex-1 overflow-auto bg-primary p-4 lg:p-8 rounded-tl-xl">
+          <main>{children}</main>
+          {/* Footer here */}
+        </div>
+      </div>
+    </div>
+  )
+}
