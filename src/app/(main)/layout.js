82e2c3e64d7970fcf445ae55d0e52492f7a56253
jasoncala <calaj@uwindsor.ca>
2024-11-07 12:40:20 -0500

    Placeholder components

--- src/app/(main)/layout.js
+++ src/app/(main)/layout.js
@@ -1,14 +1,16 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import Sidebar from "@/components/Sidebar";
 
 export default function MainLayout ({ children }) {
   return (
     <div className="flex flex-col h-screen">
       <Header/>
       <div className="flex flex-1 overflow-hidden">
-        {/* Sidebar here */}
+        <Sidebar/>
         <div className="flex-1 overflow-auto bg-primary p-4 lg:p-8 rounded-tl-xl">
           <main>{children}</main>
-          {/* Footer here */}
+          <Footer/>
         </div>
       </div>
     </div>
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,5 @@
+export default async function Sidebar() {
+    return (
+        <aside>Sidebar</aside>
+    )
+}
